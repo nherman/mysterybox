5abb1cdc6b985038d5ad14f911da7ac17aef077e
Nick Herman <nherman@gmail.com>
2014-11-24 16:09:27 -0500

    * removed settimeout & recusion in favor of a setintervals for looping

--- mysterybox.js
+++ mysterybox.js
@@ -1,3 +1,8 @@
+/***
+ * MYSTERYBOX
+ * Fill up a DOM element with random characters then have them "resolve" into something non-random
+ */
+
 window.MYSTERYBOX = window.MYSTERYBOX || (function() {
     "use strict";
 
@@ -30,7 +35,15 @@ window.MYSTERYBOX = window.MYSTERYBOX || (function() {
             }
         };
 
-    function box(options) {
+    /*
+     * Box
+     * Primary class for managing the DOM element to be manipulated.  Contains
+     * a variety of configurable defaults for controlling attributes of the
+     * text:
+     *
+     *  
+    */
+    function Box(options) {
         var 
             elmStyle,
             elmDimension = {'x':0,'y':0};
@@ -45,6 +58,9 @@ window.MYSTERYBOX = window.MYSTERYBOX || (function() {
         };
         extend(this.options, options);
 
+        /*
+         * Box emits the following events: character init, updated, all characters updated
+         */
         this.events = {
             /* do event constructors work in newer version of IE? */
             "mb_charUpdated": new Event('mb_charUpdated'),
@@ -52,44 +68,69 @@ window.MYSTERYBOX = window.MYSTERYBOX || (function() {
             "mb_init": new Event('mb_init')
         };
 
+
+        /*
+         * Get DOM element
+         */
         if (this.options.domElmId == "") {
             this.domElm = document.getElementsByTagName("body")[0];
         } else {
             this.domElm = document.getElementById(this.options.domElmId);
         }
 
+        /*
+         * Set element styles
+         */
         this.domElm.style.cssText += "display:block;unicode-bidi:embed;white-space:pre;word-wrap: break-word;font-family:monospace;";
         this.domElm.style.cssText += this.options.cssText;
 
+        /*
+         * Get box size and initialize variables
+         */
         elmStyle = window.getComputedStyle(this.domElm);
         extend(elmDimension, {
             "x": this.domElm.clientWidth - elmStyle.getPropertyValue('padding-left').match(/\d+/)[0] - elmStyle.getPropertyValue('padding-right').match(/\d+/)[0],
             "y": this.domElm.clientHeight - elmStyle.getPropertyValue('padding-top').match(/\d+/)[0] - elmStyle.getPropertyValue('padding-bottom').match(/\d+/)[0]
         });
-
         this.cols = Math.floor(elmDimension.x/this.options.charWidth);
         this.rows = Math.floor(elmDimension.y/this.options.charHeight);
-
         this.total = this.rows * this.cols;
-
         this.buffer = [];
         this.cleanChars = [];
         this.msg = this.options.message;
 
-        this.init();
-    }
+        /*
+         * initialze buffer with message
+         */
+        this.initMsg();
 
-    box.prototype.init = function(msg) {
-        if (msg !== undefined) {
-            this.msg = msg;
-        }
-        this.domElm.innerHTML = this.getRandom();
-        this.populateBuffer();
-
+        /* trigger 'init' event */
         this.trigger("mb_init");
     }
 
-    box.prototype.getRandom = function() {
+    /*
+     * initMsg
+     * set msg and populate "resolved" buffer
+     */
+    Box.prototype.initMsg = function(msg) {
+        if (msg !== undefined) {
+            this.msg = msg;
+        }
+        this.populateBuffer();
+    }
+
+    /*
+     * initRandom
+     * fill the DOM element with random characters
+     */
+    Box.prototype.initRandom = function(msg) {
+        this.domElm.innerHTML = this.getRandom();
+    }
+
+    /*
+     * get a string of random characters the length of the buffer
+     */
+    Box.prototype.getRandom = function() {
         var str = "";
         for (var i=0;i<this.total;i++) {
             str += this.getRandLatin();
@@ -97,34 +138,77 @@ window.MYSTERYBOX = window.MYSTERYBOX || (function() {
         return str;
     }
 
-    box.prototype.resolve = function(threads) {
+    /*
+     * resolve
+     * replace all charactes in the DOM element with the contents of the buffer
+     */
+    Box.prototype.resolve = function(threads) {
+        var self = this,
+            intervals = [];
+
+        /* figure out how many threads to spawn to limit the animation to a few seconds */
         if (threads === undefined) {
             threads = Math.ceil(this.total/250)
         }
+
+        /* initialize array to keep track of characters that haven't been updated */
         for (var i=0;i<this.total;i++) {
             this.cleanChars[i] = i;
         }
+
+        /* start resolving characters */
         for (var i=0;i<threads;i++) {
-            this.resolveChar(true);
+            intervals[i] = setInterval(function() {self.resolveChar()}, 0);
         }
+
+        /* when all clean characters have been resolved break the resolve loop(s) */
+        this.domElm.addEventListener('mb_allCharsUpdated',
+            function() {
+                for (var i=0; i<intervals.length; i++) {
+                    clearInterval(intervals[i]);
+                }
+            }, false);
     }
 
-    box.prototype.dissolve = function(threads) {
+    /*
+     * dissolve
+     * replace all charactes in the DOM element with random characters
+     */
+    Box.prototype.dissolve = function(threads) {
+        console.log('dissolve');
+        var self = this,
+            intervals = [];
+
+        /* figure out how many threads to spawn to limit the animation to a few seconds */
         if (threads === undefined) {
             threads = Math.ceil(this.total/250)
         }
+
+        /* initialize array to keep track of characters that haven't been updated */
         for (var i=0;i<this.total;i++) {
             this.cleanChars[i] = i;
         }
 
-        this.buffer = this.getRandom();
-
+        /* start inserting random characters in random positions */
         for (var i=0;i<threads;i++) {
-            this.resolveChar(true);
+            intervals[i] = setInterval(function() {self.resolveChar(self.getRandLatin())}, 0);
         }
+
+        /* when all clean characters have been resolved break the resolve loop(s) */
+        this.domElm.addEventListener('mb_allCharsUpdated',
+            function() {
+                for (var i=0; i<intervals.length; i++) {
+                    clearInterval(intervals[i]);
+                }
+            }, false);
     }
 
-    box.prototype.resolveChar = function(recurse) {
+    /*
+     * resolveChar
+     * randomly select a "clean" character from the DOM element and replace
+     * it with the matching character from the buffer OR the character passed in
+     */
+    Box.prototype.resolveChar = function(c) {
         if (this.cleanChars.length) {
             var self = this,
                 j = Math.floor(Math.random() * this.cleanChars.length),
@@ -132,11 +216,10 @@ window.MYSTERYBOX = window.MYSTERYBOX || (function() {
 
             this.cleanChars.splice(j,1);
 
-            this.domElm.innerHTML = this.domElm.innerHTML.substring(0,i) + this.buffer[i] + this.domElm.innerHTML.substring(i+1);
-
-            if (recurse !== undefined && recurse) {
-                setTimeout(function() {self.resolveChar(true)}, 0);
+            if (c === undefined) {
+                c = this.buffer[i];
             }
+            this.domElm.innerHTML = this.domElm.innerHTML.substring(0,i) + c + this.domElm.innerHTML.substring(i+1);
 
             this.trigger("mb_charUpdated");
         } else {
@@ -144,7 +227,12 @@ window.MYSTERYBOX = window.MYSTERYBOX || (function() {
         }
     }
 
-    box.prototype.populateBuffer = function() {
+    /*
+     * populateBuffer
+     * insert all characters from this.msg into this.buffer.
+     * format the text based on configuration
+     */
+    Box.prototype.populateBuffer = function() {
         var msgRows = Math.ceil(this.msg.length/Math.min(this.options.msgLineMaxWidth, this.cols)),
             rowLength = Math.ceil(this.msg.length/msgRows),
             msgArr = this.msg.split(" "),
@@ -182,19 +270,21 @@ window.MYSTERYBOX = window.MYSTERYBOX || (function() {
         }
     }
 
-    box.prototype.getRandLatin = function() {
+    /* return a random latin character */
+    Box.prototype.getRandLatin = function() {
         //return String.fromCharCode(0x30A0 + Math.random() * (0x30FF-0x30A0+1));   //katakana
         //return String.fromCharCode(0x0041 + Math.random() * (0x005A-0x0041+1));   //uppercase A-Z
         return String.fromCharCode(0x0061 + Math.random() * (0x007A-0x0061));      //lowercase a-z
     }
 
-    box.prototype.trigger = function(eventName) {
+    /* dispatch an event */
+    Box.prototype.trigger = function(eventName) {
         this.domElm.dispatchEvent(this.events[eventName]);
     }
 
     /*
-    * expose box class
+    * expose Box class
     */
-    return box;
+    return Box;
 
 }());
